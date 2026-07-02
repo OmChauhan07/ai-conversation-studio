6ccadcfe914f9d422ab5c6e7a99fec65e1871098
OmChauhan07 <odchauhan0702@gmail.com>
2026-07-03 02:09:46 +0530

    Update app.js

--- backend-auth/src/app.js
+++ backend-auth/src/app.js
@@ -8,7 +8,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [
+      "http://localhost:5173",
+      ""
+    ],
     credentials: true,
   })
 );
